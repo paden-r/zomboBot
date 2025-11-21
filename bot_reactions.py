5e40c8680c92273539b49e50e8de622b857d6199
Paden Rose <prose@Mac.localdomain>
2025-11-20 21:31:25 -0600

    Adding new reaction

--- bot_reactions.py
+++ bot_reactions.py
@@ -3,7 +3,7 @@ import discord
 
 from pathlib import Path
 from dotenv import load_dotenv
-from constants import PANIK
+from constants import PANIK, AMERICA
 
 dotenv_path = Path(".env_secret")
 load_dotenv(dotenv_path=dotenv_path)
@@ -18,8 +18,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if 'omg' in message.content.lower():
+    this_message = message.content.lower()
+
+    if 'omg' in this_message:
         await message.channel.send(PANIK)
+
+    if 'america' in this_message:
+        await message.channel.send(AMERICA)
 
 
 client.run(TOKEN)
